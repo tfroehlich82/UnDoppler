3c88a4c55ae9e94ae9934efb29f880681b4364de
Thorsten Fröhlich <thorsten.froehlich@metromec.ch>
2017-06-06 13:09:06 +0200

    Added more skeletons

--- undoppler/lib/comparisons.py
+++ undoppler/lib/comparisons.py
@@ -6,3 +6,26 @@ def compare_binary(file_1, file_2):
     if data1 != data2:
         return False
     return True
+
+
+def is_in(pattern, file_1):
+    with open(file_1, 'rb') as file1:
+        data1 = file1.read()
+
+    return pattern in data1
+
+
+def same_filesize():
+    pass
+
+
+def same_datetime():
+    pass
+
+
+def same_filename():
+    pass
+
+
+def same_extension():
+    pass
